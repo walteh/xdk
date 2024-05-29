75d7d4fd8ca9e48ed14bd844cd75b02ac78b65b1
Walter Scott <w@lter.ca>
2024-05-29 14:40:10 -0500

    add better support for webview

--- Sources/Packages/awssso/swift/UserSession.swift
+++ Sources/Packages/awssso/swift/UserSession.swift
@@ -21,23 +21,25 @@ public class Viewer: NSObject, ObservableObject, WKNavigationDelegate {
 	public var currentURL: URL? = nil
 
 	public let managedRegion: ManagedRegion
+	public let account: AccountInfo
 	public let webview: WKWebView
 
 	public let rebuildURL: () async -> Void
 
-	public init(webview: WKWebView, session: AWSSSOUserSession, storageAPI: some XDK.StorageAPI) {
+	public init(webview: WKWebView, session: AWSSSOUserSession, storageAPI: some XDK.StorageAPI, account: AccountInfo) {
 		self.webview = webview
 		self.managedRegion = session
-
+		self.account = account
 		self.rebuildURL = {
 			var err: Error? = nil
+
 
 			guard let awsClient = XDKAWSSSO.buildAWSSSOSDKProtocolWrapped(ssoRegion: session.accessToken!.region).to(&err) else {
 				XDK.Log(.error).err(err).send("error generating console url")
 				return
 			}
 
-			guard let url = await XDKAWSSSO.generateAWSConsoleURL(client: awsClient, account: session.currentAccount!, managedRegion: session, storageAPI: storageAPI, accessToken: session.accessToken!).to(&err) else {
+			guard let url = await XDKAWSSSO.generateAWSConsoleURL(client: awsClient, account: account, managedRegion: session, storageAPI: storageAPI, accessToken: session.accessToken!).to(&err) else {
 				XDK.Log(.error).err(err).send("error generating console url")
 				return
 			}
@@ -45,6 +47,8 @@ public class Viewer: NSObject, ObservableObject, WKNavigationDelegate {
 				XDK.Log(.error).err(err).send("error configuring cookies")
 				return
 			}
+
+
 //			DispatchQueue.main.async {
 			guard let _ = await webview.load(URLRequest(url: url)) else {
 				XDK.Log(.error).send("error loading webview")
@@ -79,7 +83,9 @@ public class Viewer: NSObject, ObservableObject, WKNavigationDelegate {
 		// constructSimpleConsoleURL(region: session.region ?? session.accessToken?.region ?? "us-east-1", service: session.service)
 	}
 }
+// https://us-east-1.signin.aws.amazon.com/federation?Action=getSigninToken&sessionDuration=3200&Session=%7B%22sessionToken%22:%22IQoJb3JpZ2luX2VjEJP__________wEaCXVzLWVhc3QtMSJHMEUCIQCoEp_Fz4xKByFeAAFyEiSbnP5GpYeSTGVIJ8Q-cQHLlQIgP4v_n1q_81T5KdoUUAV9O6HURzP6KEPi55e-_8fTd48qwwMIGxAAGgw4NjAwNzg3NTI5MTUiDJ28J1j4pui2AKQ-7iqgA3JlCxcLUZCDrDCcPcc7pe2FUKJPLwxfUPzQXh0vR1bd6m90TN1koZi7_w0IIC6TYShbTeivMcTOG36xCiidglzJPXtuXdsO-gcSp7SHpLsGKT3K91uUbYMEse-wEegWkPwzkXFjFXvu1FKCvGYCS50TMgedFXc6NT3g9FfWQ20v9EuJ_YZVV9N81dPWHHL4t5NzPy9glSp4JtQo4MYWt8mk-VnFqF-P3vDU2Qem_SOaShyw85P1nPbqFK6ZytkSVxszC5-RDtZmO9bWsfx-W9ttaFPIwgrDvASj8xbbjWjZoodGOXQnHAR32MMB3OJKZLdiwN2uc9-znAO8dspzhbGLK70dlegOKYX5tCl4tXS8AL_I_zLY-tINHz4H15h9dpCOxkxjvGVrC478kDbnRTIjvba5fSdUuKG6jWMqnHP2EVSV8ghzGcOKYuuD1FbUQZhZ182vRJciolI-8bcylIwV1BphFZbmCB2agBrOWAAHESCw8Z2zTHV-HbueWgy7ZTKzea8XHO31v3C38lpGPQUK1QueRq_PmP08R3MoWBCgMPvh3bIGOqYBFIf9RNPN_4S2SD26wbTu2SEjblm6Mofs2-jZzjMI7Y9VmGER34XptIAQvyVrtSqu8B5AldD1b-x_upq4MM70cmRhdk7an38dxpRwg5uVRxLtNtpg4TEkWu98QADro0ucAOk2k53mlRzyQFKppC_d7QIWsfAyathRVUg5z40n3N7ozP_wF3si_SABY1yE7k-QGCyyDfNvnRm068i2HOGOCTqkuB5Iuw%22,%22sessionId%22:%22ASIA4QQFQISJ5WXXPKQS%22,%22sessionKey%22:%22Mocc+pCDiPLC3bT0lBwUAfPt9B13HZ+84isxnMzK%22%7D
 
+// https://us-east-1.signin.aws.amazon.com/federation?Action=login&Issuer=com.walteh.spatial-aws&Destination=https://us-east-1.console.aws.amazon.com/console/home?region%3Dus-east-1&SigninToken=fhlgPIrJcvBT1liYm3wxh2uKlHr3su-f_E4oN_Za5A2YCaoxBZcC1y7kpM6sobBi3eElZZt-V4fvmimVR6uP-4hZ-MNVICKVmHrPTju6xRy6Bpdi9Zqul_NzQc8Qn1aAPJGIRu6ujRuOKmIzVxE3ZdRVPvOoMkeJXZXeSqJ9WHjuZXpx-MfId48pXxkBxXJ_ieniAE77xwMfBIRYBel0o7UDVmZ35HBpoVLhcdWHC5OVBcSW8BnpFKHvZ8tQj-vvXRP8yPJg3oVEModK7Q3ajH85jEPV62Aogf1rhPiJRS-kLtocJzqYYPDszZ_zwAB8SRlZxmw-7uHw2FKQwyMY9nx0fhyrCo9ZQ_paf531NWJS_61E75bm4clCL_gTPR4PUzQHo2daaCtDd2odXhBIMrgSNfIM1O0OoiupzlGvSPDP0dWWK2YRvHBCdR2H7t65I54ttsN3TO9Q2h8uOmCxqAF_BXEsMBhHukKod1hbAuQ3JYCELX5HkE0Hi-BMSkq4wJp_qMdF023RouRqFVuaMeIirskPEjRw7ostcA_WtenUVz1LIA9j7W87823PVX5ijFIdEsbXJrJEdB0kG7kWTf0nFiYOEFFJCsGj2gh8_i1N7ahO_yupWMN9GiXbtStd5zWSGW0pk_K6l56jAh9PjrQXkqjgMuGwNsA9t7vQBnTkzldUCe8U-8NH2Vrj-tKyjAoWUflGW30_ETR8LlHLrSec03GD7N9m7Y0LBqPLqAARapy7f7Bgv-UPWzJGYFpggmEAj2kniL_NoRgGTvkYz9A_cr5avEVpMcDPpRNJy2yE4IAczP1Zx2pWEKh9u6503HU3xkor-rH1JbVZKlcUwaBcEcKdKdIAfYSkmffCWTUuiFu5VRO1CsE5oENA5mItVrKWv49uBkMcufOayR_fOviKPvpamkQLkB2dPWGZyDujE1jFWSLrMw_oNAVKAOBaoXkSTydpPT_R9pduZCL3i1K_WdVQYCUXQ3j-jxTId9obQGiq16i33Z0fiYuaRsftbfV6ZCkTJtnJr2YZ50EWvva8bZu7YuiSaaNivfv9wShSA9ltNQRIhNvBYcq5Zn4NAfctuZWwpPKWmGBcrbkgFB_AA_ZUpIZ6Bf4tSk8_TS0EUg-Bc60iiz_MYuD7o5EOXVH6V4sUypQSypJWFOXPWLL4sebAuMxJSRq66vm5XjKAJ_8J-NkP5rm_Llcoxd6qhPVuU3yS4bao_WmdPvTy5Mst59t9fpK6oRsyjiwb6rfbc4dTgARJ2ipVfxpF5T2goCi5fNSLK6VVopSn6zHjL-eOcLEHIX4rRBzS46VgfHLojTJj-l6OcBvcdYw1F2ZLRdJlf0nqwtm9SE0S9rS5QKFJNln_mlSWc-begxEoPEo_GiqeRbVJ1xAmr-Kq4S6uS7QY_XKUppVkf1v5ZHlAXIe5zQvgqHMSerLGzBGX4DrrpmbcxwvwlvuGg3Qwvb4ZNqY2GL90kP_E577xnpTy4_gcSQ_wejHRGcU98q2phHzzseixl-Dns-GFx4UG2nb0H1iuGsxYbIR5qRudAEnO-OxEvPtNEsQpEvVBmCtOBMeKClndBqZvCzpcL0s_KZGQfAnk27aTAP0QW-rcvOkNsmx7RWV44Dy6zXBQVsuJa57cOsoDR8btyDHXfrTsZXPFaRQN88_z-n_-O4V1PCwxXH7pcVn2y4Al-GFQn3ZlCkJT04fM5dlsi8rr4mSofYq7LJY7k2aXv8-rkQ2LQdJWI5FlocbM9w
 public class AWSSSOUserSession: ObservableObject, ManagedRegion {
 	public var accessTokenPublisher: Published<SecureAWSSSOAccessToken?>.Publisher { self.$accessToken }
 	@Published public var accessToken: SecureAWSSSOAccessToken? = nil
@@ -89,9 +95,30 @@ public class AWSSSOUserSession: ObservableObject, ManagedRegion {
 
 	@Published public var accounts: [AccountInfo: Viewer] = [:]
 
-	@Published public var region: String?
 
-	@Published public var service: String? = nil
+	@Published public var region: String?  {
+		didSet {
+			DispatchQueue.main.async {
+				for (_, viewer) in self.accounts {
+					Task {
+						await viewer.rebuildURL()
+					}
+				}
+			}
+		}
+	}
+
+	@Published public var service: String? = nil {
+		didSet {
+			DispatchQueue.main.async {
+				for (_, viewer) in self.accounts {
+					Task {
+						await viewer.rebuildURL()
+					}
+				}
+			}
+		}
+	}
 
 	let storageAPI: any XDK.StorageAPI
 
@@ -102,7 +129,8 @@ public class AWSSSOUserSession: ObservableObject, ManagedRegion {
 			if let wk = self.accounts[currentAccount] {
 				return wk.webview
 			} else {
-				self.accounts[currentAccount] = Viewer(webview: createWebView(), session: self, storageAPI: self.storageAPI)
+				let viewer = Viewer(webview: createWebView(), session: self, storageAPI: self.storageAPI, account: currentAccount)
+				self.accounts[currentAccount] = viewer
 				return self.accounts[currentAccount]!.webview
 			}
 		}
@@ -262,12 +290,12 @@ public func generateAWSConsoleURL(client: AWSSSOSDKProtocolWrapped, account: Acc
 	return .success(consoleHomeURL)
 }
 
-func constructFederationURLRequest(with credentials: RoleCredentials, region: String) -> Result<URLRequest, Error> {
+func constructFederationURLRequest(with credentials: RoleCredentials) -> Result<URLRequest, Error> {
 	var err: Error? = nil
 
 	let federationBaseURL = credentials.stsRegion.starts(with: "us-gov-") ?
 		"https://signin.amazonaws-us-gov.com/federation" :
-		"https://\(region).signin.aws.amazon.com/federation"
+	"https://\(credentials.stsRegion).signin.aws.amazon.com/federation"
 
 	guard let sessionStringJSON = Result.X({ try JSONEncoder().encode([
 		"sessionId": credentials.accessKeyID,
@@ -295,7 +323,7 @@ func constructFederationURLRequest(with credentials: RoleCredentials, region: St
 func fetchSignInToken(with credentials: RoleCredentials) async -> Result<String, Error> {
 	var err: Error? = nil
 
-	guard let request = constructFederationURLRequest(with: credentials, region: credentials.stsRegion).to(&err) else {
+	guard let request = constructFederationURLRequest(with: credentials).to(&err) else {
 		return .failure(x.error("error constructing federation url", root: err))
 	}
 
@@ -352,7 +380,7 @@ func constructSimpleConsoleURL(region: String, service: String? = nil) -> Result
 func constructLoginURL(with signInToken: String, credentials: RoleCredentials, region: String, service: String?) -> Result<URL, Error> {
 	var err: Error? = nil
 
-	guard let request = constructFederationURLRequest(with: credentials, region: region).to(&err) else {
+	guard let request = constructFederationURLRequest(with: credentials).to(&err) else {
 		return .failure(x.error("error constructing federation url", root: err))
 	}
 
